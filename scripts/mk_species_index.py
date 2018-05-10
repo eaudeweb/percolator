78baeca05280a94902afe7e2ede512e5c0fdac97
Andrei Duhnea <andrei@duhnea.net>
2018-05-10 15:19:34 +0300

    Script updates

--- scripts/mk_species_index.py
+++ scripts/mk_species_index.py
@@ -3,15 +3,12 @@
 import sys
 import logging
 from elasticsearch_dsl.connections import connections
-from percolator.search import SpeciesTagger
+from percolator.search.species import SpeciesQueryIndexer
 
 log = logging.getLogger('percolator_search')
 log.setLevel(logging.INFO)
 
 client = connections.create_connection(hosts=['localhost'], timeout=20)
-tagger = SpeciesTagger(client=client)
+indexer = SpeciesQueryIndexer(client=client)
 if len(sys.argv) > 1:
-    with open(sys.argv[1], 'r') as f:
-        tags = tagger.get_tags(f.read())
-        for tag in tags:
-            print(f'{tag[0]:50} ({tag[1]:>.2f})')
+    indexer.register_queries(tags_path=sys.argv[1])
