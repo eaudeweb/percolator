cf357682a86f09f7f21a5937c0e68966f1318ad9
Andrei Duhnea <andrei@duhnea.net>
2018-05-14 14:48:21 +0300

    Parameter validation in tag endpoint

--- percolator/api/views.py
+++ percolator/api/views.py
@@ -1,19 +1,31 @@
-from apistar import http
+from apistar import types, validators
 from ..search import BaseTagger, SpeciesQueryIndexer
 from elasticsearch import Elasticsearch
 
+from percolator.conf import settings
+
+
+class TagQuery(types.Type):
+    """
+    Validator for `tag` endpoint request data.
+    """
+    content = validators.String(max_length=1024 * 1024 * 10)
+    offset = validators.Integer(minimum=0, allow_null=True)
+    limit = validators.Integer(minimum=1, allow_null=True)
+    min_score = validators.Number(minimum=0.1, allow_null=True)
+    constant_score = validators.Boolean(default=True, allow_null=True)
+
 
 def tag(
-    data: http.RequestData,
-    offset: http.QueryParam,
-    limit: http.QueryParam,
-    min_score: http.QueryParam,
+    query: TagQuery,
     es_client: Elasticsearch
 ) -> dict:
-    tagger = BaseTagger(indexer=SpeciesQueryIndexer(client=es_client))
+    indexer = SpeciesQueryIndexer(client=es_client, index=settings.ELASTICSEARCH_INDEX)
+    tagger = BaseTagger(indexer=indexer)
     tags = tagger.get_tags(
-        content=data.get('content'),
-        min_score=min_score, offset=offset, limit=limit
+        content=query.content,
+        min_score=query.min_score, constant_score=query.constant_score,
+        offset=query.offset, limit=query.limit
     )
-    response = {t[0]: t[1] for t in tags}
-    return response
+
+    return tags
