183ff99b648539eb52dff8d92fec9dd877db8a8f
Andrei Duhnea <andrei@duhnea.net>
2018-05-14 14:47:36 +0300

    Update index creation script

--- scripts/mk_species_index.py
+++ scripts/mk_species_index.py
@@ -5,10 +5,11 @@ import logging
 from elasticsearch_dsl.connections import connections
 from percolator.search.species import SpeciesQueryIndexer
 
+logging.basicConfig()
 log = logging.getLogger('percolator_search')
 log.setLevel(logging.INFO)
 
 client = connections.create_connection(hosts=['localhost'], timeout=20)
-indexer = SpeciesQueryIndexer(client=client)
+indexer = SpeciesQueryIndexer(client=client, index='species_percolator')
 if len(sys.argv) > 1:
     indexer.index_queries(tags_path=sys.argv[1])
