8576d8b2aa0d9c86398cc4b7673afe5f1c5bbab0
Andrei Duhnea <andrei@duhnea.net>
2018-05-15 16:20:26 +0300

    Collect apistar statics in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Copy apistar package static files
+mkdir -p static/apistar
+cp -r /usr/local/lib/python3.6/site-packages/apistar/static/* static/apistar
+
 case "$1" in
     build_index)
         exec python scripts/mk_species_index.py data/samples/species.txt
