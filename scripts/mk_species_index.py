4c7996611f39c85184baba4e87960f425f9ffdfc
Andrei Duhnea <andrei@duhnea.net>
2018-05-15 16:18:38 +0300

    Use settings ES hosts in indexing script

--- scripts/mk_species_index.py
+++ scripts/mk_species_index.py
@@ -3,13 +3,14 @@
 import sys
 import logging
 from elasticsearch_dsl.connections import connections
+from percolator.conf import settings
 from percolator.search.species import SpeciesQueryIndexer
 
 logging.basicConfig()
 log = logging.getLogger('percolator_search')
 log.setLevel(logging.INFO)
 
-client = connections.create_connection(hosts=['localhost'], timeout=20)
+client = connections.create_connection(hosts=settings.ELASTICSEARCH_HOSTS, timeout=20)
 indexer = SpeciesQueryIndexer(client=client, index='species_percolator')
 if len(sys.argv) > 1:
     indexer.index_queries(tags_path=sys.argv[1])
