7cb91fc8b714b80eb54d24da0e86c72de432ca6f
Andrei Duhnea <andrei@duhnea.net>
2018-05-21 16:39:00 +0300

    Add multipart/form-data components #10

--- percolator/api/__init__.py
+++ percolator/api/__init__.py
@@ -1,11 +1,17 @@
 from apistar import App
 
 from .routes import routes
-from .components import ElasticSearchClientComponent
+from .components import (
+    ElasticSearchClientComponent,
+    RequestStreamComponent,
+    MultiPartParserComponent,
+)
 from percolator.conf import settings
 
 components = [
-    ElasticSearchClientComponent(hosts=settings.ELASTICSEARCH_HOSTS)
+    ElasticSearchClientComponent(hosts=settings.ELASTICSEARCH_HOSTS),
+    RequestStreamComponent(),
+    MultiPartParserComponent(),
 ]
 
 app = App(routes=routes, components=components, static_dir=settings.STATIC_DIR)
--- percolator/api/components.py
+++ percolator/api/components.py
@@ -1,5 +1,14 @@
 import logging
+import io
+import typing
+from apistar import http
+from apistar.server.wsgi import WSGIEnviron
 from apistar.server.components import Component
+from werkzeug.datastructures import ImmutableMultiDict
+from werkzeug.formparser import FormDataParser
+from werkzeug.http import parse_options_header
+from werkzeug.wsgi import get_input_stream
+
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl.connections import connections
 
@@ -14,3 +23,36 @@ class ElasticSearchClientComponent(Component):
 
     def resolve(self) -> Elasticsearch:
         return self.client
+
+
+RequestStream = typing.NewType('RequestStream', io.BufferedIOBase)
+MultiPartForm = typing.NewType('MultiPartForm', ImmutableMultiDict)
+
+
+class RequestStreamComponent(Component):
+    def resolve(self, environ: WSGIEnviron) -> RequestStream:
+        return get_input_stream(environ)
+
+
+class MultiPartParserComponent(Component):
+
+    @staticmethod
+    def _get_content_length(headers: http.Headers) -> typing.Optional[int]:
+        content_length = headers.get('Content-Length')
+        if content_length is not None:
+            try:
+                return max(0, int(content_length))
+            except (ValueError, TypeError):
+                pass
+        return None
+
+    @staticmethod
+    def _get_mimetype_and_options(headers: http.Headers) -> typing.Tuple[str, dict]:
+        return parse_options_header(headers.get('Content-Type'))
+
+    def resolve(self, headers: http.Headers, stream: RequestStream) -> MultiPartForm:
+        mimetype, options = self._get_mimetype_and_options(headers)
+        content_length = self._get_content_length(headers)
+        parser = FormDataParser()
+        stream, form, files = parser.parse(stream, mimetype, content_length, options)
+        return ImmutableMultiDict(list(form.items()) + list(files.items()))
